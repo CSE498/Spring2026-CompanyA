ee703a316baf594fc6a7520649e6cd894102d1a7
Sitara Kaur Baxendale <baxenda5@msu.edu>
2026-02-11 13:17:55 -0500

    added SDL library (& updated Makefiles), made ImageManager functional, and created ImageManager test suite

--- source/Interfaces/GUI/ImageManager.hpp
+++ source/Interfaces/GUI/ImageManager.hpp
@@ -2,10 +2,16 @@
  * @file ImageManager.hpp
  * @author Sitara Baxendale
  *
+ * Class that handles image assets by name, preloading them and
+ * displaying them on the screen as requested
  *
  */
  
-#pragma once
+#ifndef IMAGEMANAGER_HPP
+#define IMAGEMANAGER_HPP
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 #include <string>
 #include <unordered_map>
@@ -14,18 +20,29 @@ namespace cse498 {
 
     class ImageManager {
     private:
-        // map image name to file path
-        std::unordered_map<std::string, std::string> mImage;
+        // renderer for images
+        SDL_Renderer* mRenderer;
+
+        // map to store & associate images names to textures
+        std::unordered_map<std::string, SDL_Texture*> mTextures;
 
     public:
-        ImageManager();
+        // constructor & destructor
+        explicit ImageManager(SDL_Renderer* renderer);
         ~ImageManager();
 
+        // cannot be copied
+        ImageManager(const ImageManager&) = delete;
+    	ImageManager& operator=(const ImageManager&) = delete;
+
+        // class functions
         void load_image(const std::string& name, const std::string& file_path);
-        std::string get_image(const std::string& name);
-        void draw_image(const std::string& name, int x, int y);
-        bool has_image(const std::string& name);
+        bool has_image(const std::string& name) const;
+        bool draw_image(const std::string& name, int x, int y) const;
+        SDL_Texture* get_texture(const std::string& name) const;
 
     };
 
 }
+
+#endif
