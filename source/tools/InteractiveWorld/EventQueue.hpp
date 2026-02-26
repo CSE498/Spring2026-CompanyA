2be8b861cfa67297cd928b9123103657096b98ff
hoho <hoho@msu.edu>
2026-02-26 01:41:28 -0500

    improve EventQueue

--- source/tools/InteractiveWorld/EventQueue.hpp
+++ source/tools/InteractiveWorld/EventQueue.hpp
@@ -42,7 +42,7 @@ namespace cse498 {
     // Main event heap (top = next event)
     std::priority_queue<Event> m_events;
 
-    // IDs that are marked cancelled (lazy cancellation)
+    // IDs that are marked cancelled
     std::unordered_set<EventID> m_cancelled_ids;
 
     // IDs that are currently scheduled and not yet popped/removed
@@ -53,19 +53,18 @@ namespace cse498 {
     std::uint64_t m_next_order = 0;
 
     /// @brief Remove cancelled events sitting at the top of the heap.
-    /// If we pop a cancelled event, we also clean its id from sets.
     void CleanTop() {
       while (!m_events.empty()) {
         const Event& top = m_events.top();
 
-        // If top event isn't cancelled, stop cleaning.
+        // If top event isn't cancelled, stop remove.
         if (m_cancelled_ids.count(top.id) == 0) break;
 
         // Top event is cancelled -> remove it from heap
         const EventID cancelled_id = top.id;
         m_events.pop();
 
-        // Fully clean the id
+        // Fully release the id
         m_cancelled_ids.erase(cancelled_id);
         m_active_ids.erase(cancelled_id);
       }
@@ -119,7 +118,7 @@ namespace cse498 {
       // User error: cancelling an id that doesn't exist / not active
       if (m_active_ids.count(id) == 0) return false;
 
-      // Mark as cancelled (lazy). If already cancelled, insert will fail.
+      // Mark as cancelled. If already cancelled, insert will fail.
       return m_cancelled_ids.insert(id).second;
     }
 
