620f66fce075d86819618e8fdab605883a4e3b33
finfroc3 <finfroc3@msu.edu>
2026-02-27 01:30:20 -0500

    MemoFunction review changes
    name changes, better cache eviction, use of std::optional for mLimit, more exhaustive limit testing

--- source/tools/MemoFunction.hpp
+++ source/tools/MemoFunction.hpp
@@ -3,16 +3,16 @@
  * @author Henry Finfrock
  *
  * This class acts as a function wrapper, caching outputs to improve performance
- * Initial implementation, subject to change
+ * Initial post-review implementation, subject to change
  */
 
 #pragma once
 
+#include <optional>
 #include <unordered_map>
 #include <map>
 #include <functional>
 #include <deque>
-//#include <iostream>
 
 namespace cse498 {
 
@@ -24,45 +24,46 @@ namespace cse498 {
   // Key type so multiple inputs can be used as a cache key
   using Key = std::tuple<I...>;
 
-  // Cache to contain function outputs (should probably be unordered
-  // for performance, doing map for ease of use for now)
+  std::function<R(I...)> mWrappedFunction;
+
+  // Cache to contain function outputs (ordered map for now,
+  // look into using RobinHoodMap)
   std::map<Key, R> mCache;
 
   // Queue that records order of caching, so when the cache reaches its
   // limit it will remove the oldest
   std::deque<Key> mQueue;
 
-  // The function being wrapped
-  std::function<R(I...)> mFunction;
+  // Size limit for cache (0 being no limit)
+  std::optional<size_t> mLimit = std::nullopt;
 
-  // Size limit for cache (0 and less being no limit)
-  size_t mLimit = 0;
-
- public:
-  // Constructor, instantiated as MemoFunction<R, I1, I2, ... , IN> name(function)
-  explicit MemoFunction(std::function<R(I...)> mFunction) : mFunction(mFunction) {}
-
-  // Main memoization function with cache deletion when size limit is reached
-  R operator()(I... inputs)
+  // Removes oldest items until size limit is no longer exceeded
+  void EvictCache()
   {
-   Key key(inputs...);
-
-   if (mCache.find(key) != mCache.end())
+   while (mLimit && CacheSize() > *mLimit)
    {
-    //std::cout << "from cache" << std::endl;
-    return mCache[key];
-   }
-
-   R output = mFunction(inputs...);
-   mCache[key] = output;
-   mQueue.push_back(key);
-   //std::cout << "from function" << std::endl;
-
-   if (mLimit > 0 && CacheSize() > mLimit) {
     Key oldest = mQueue.front();
     mQueue.pop_front();
     mCache.erase(oldest);
    }
+  }
+
+ public:
+  // Constructor, instantiated as MemoFunction<R, I1, I2, ... , IN> name(function)
+  explicit MemoFunction(std::function<R(I...)> mWrappedFunction) : mWrappedFunction(mWrappedFunction) {}
+
+  // Main memoization function with cache eviction when size limit is reached
+  R operator()(I... inputs)
+  {
+   Key key(inputs...);
+
+   if (mCache.find(key) != mCache.end()){ return mCache[key]; }
+
+   R output = mWrappedFunction(inputs...);
+   mCache[key] = output;
+   mQueue.push_back(key);
+
+   EvictCache();
 
    return output;
   }
@@ -71,7 +72,9 @@ namespace cse498 {
 
   void CacheClear() { mCache.clear(); mQueue.clear(); }
 
-  void SetLimit(int limit) { mLimit = limit; }
+  void SetLimit(size_t limit) { mLimit = limit; EvictCache(); }
+
+  void RemoveLimit() { mLimit = std::nullopt; }
 
   bool IsCached(Key key) const { return mCache.find(key) != mCache.end(); }
  };
--- tests/tools/TestMemoFunction.cpp
+++ tests/tools/TestMemoFunction.cpp
@@ -12,8 +12,8 @@
 #include <vector>
 #include <chrono>
 #include <iostream>
+#include <climits>
 
-using namespace cse498;
 using namespace std::chrono;
 using std::string, std::vector;
 
@@ -21,7 +21,7 @@ int multby3(int x) { return x * 3; }
 
 TEST_CASE("MemoFunction: Basic function and cache testing", "[MemoFunction]")
 {
-    MemoFunction<int, int> mf1(multby3);
+    cse498::MemoFunction<int, int> mf1(multby3);
     REQUIRE(mf1.CacheSize() == 0);
     REQUIRE(mf1(7) == 21);
     REQUIRE(mf1.CacheSize() == 1);
@@ -49,18 +49,18 @@ TEST_CASE("MemoFunction: Class member functions w/ multiple inputs and types", "
 {
     testclass t1;
     auto l1 = [&](string a, string b) { return t1.concat(a, b); };
-    MemoFunction<string, string, string> mf2(l1);
+    cse498::MemoFunction<string, string, string> mf2(l1);
     REQUIRE(mf2("abc", "def") == "abcdef");
     REQUIRE(mf2.CacheSize() == 1);
 
     auto l2 = [&](int x) { return t1.even(x); };
-    MemoFunction<bool, int> mf3(l2);
+    cse498::MemoFunction<bool, int> mf3(l2);
     REQUIRE(mf3(2));
     REQUIRE(!mf3(3));
     REQUIRE(mf3.CacheSize() == 2);
 
     auto l3 = [&](double a, double b, int c) { return t1.add(a, b, c); };
-    MemoFunction<double, double, double, int> mf4(l3);
+    cse498::MemoFunction<double, double, double, int> mf4(l3);
     REQUIRE(mf4.CacheSize() == 0);
     REQUIRE(mf4(2.2, 3.6, 1) == Approx(6.8));
     REQUIRE(mf4.CacheSize() == 1);
@@ -74,7 +74,7 @@ vector<char> hello() { return {'h', 'e', 'l', 'l', 'o'}; }
 
 TEST_CASE("MemoFunction: No argument", "[Memofunction]")
 {
-    MemoFunction<vector<char>> mf5(hello);
+    cse498::MemoFunction<vector<char>> mf5(hello);
     auto v = mf5();
     REQUIRE(string(v.begin(), v.end()) == "hello");
     REQUIRE(mf5.CacheSize() == 1);
@@ -84,12 +84,12 @@ int exception() { throw std::logic_error("test"); return 1; }
 
 TEST_CASE("MemoFunction: Exceptions", "[MemoFunction]")
 {
-    MemoFunction<int> mf6(exception);
+    cse498::MemoFunction<int> mf6(exception);
     REQUIRE_THROWS_AS(mf6(), std::logic_error);
     REQUIRE(mf6.CacheSize() == 0);
 
     std::function<int()> f;
-    MemoFunction<int> mf7(f);
+    cse498::MemoFunction<int> mf7(f);
     REQUIRE_THROWS_AS(mf7(), std::bad_function_call);
     REQUIRE(mf7.CacheSize() == 0);
 }
@@ -104,7 +104,7 @@ int fib(int x) {
 
 TEST_CASE("MemoFunction: Performance", "[MemoFunction]")
 {
-    MemoFunction<int, int> mf8(fib);
+    cse498::MemoFunction<int, int> mf8(fib);
 
     auto start1 = high_resolution_clock::now();
     mf8(30);
@@ -126,9 +126,10 @@ TEST_CASE("MemoFunction: Performance", "[MemoFunction]")
 
 TEST_CASE("MemoFunction: Cache limit", "[MemoFunction]")
 {
-    MemoFunction<int, int> mf9(multby3);
+    cse498::MemoFunction<int, int> mf9(multby3);
     mf9.SetLimit(2);
     mf9(1); // cache = {1}
+    REQUIRE(mf9(1) == 3);
     REQUIRE(mf9.CacheSize() == 1);
     mf9(2); // cache = {1, 2}
     REQUIRE(mf9.CacheSize() == 2);
@@ -144,4 +145,35 @@ TEST_CASE("MemoFunction: Cache limit", "[MemoFunction]")
     REQUIRE(mf9.CacheSize() == 0);
     mf9(1); // cache = {1}
     REQUIRE(mf9.CacheSize() == 1);
+    mf9.SetLimit(6);
+    mf9(2);
+    mf9(3);
+    mf9(4);
+    mf9(5);
+    mf9(6); // cache = {1, 2, 3, 4, 5, 6}
+    REQUIRE(mf9.CacheSize() == 6);
+    mf9.SetLimit(3); // SetLimit now triggers eviction, which now evicts more than one
+    REQUIRE(mf9.CacheSize() == 3);
+    REQUIRE(!mf9.IsCached(1));
+    REQUIRE(mf9.IsCached(4));
+    REQUIRE(mf9.IsCached(5));
+    REQUIRE(mf9.IsCached(6));
+    mf9(7); // cache = {5, 6, 7}
+    REQUIRE(mf9.CacheSize() == 3);
+    REQUIRE(!mf9.IsCached(4));
+    mf9.RemoveLimit();
+    mf9(1);
+    REQUIRE(mf9.CacheSize() == 4);
+    mf9.SetLimit(0); // cache has to be empty
+    REQUIRE(mf9.CacheSize() == 0);
+    mf9(1);
+    REQUIRE(mf9.CacheSize() == 0);
+    mf9.SetLimit(-1); // wraps around and sets it to max size_t value
+    for (int i = 0; i < 10000; i++) {
+      mf9(i);
+    }
+    REQUIRE(mf9.CacheSize() == 10000); // doesnt prove much but shows limit is set high
+    mf9.SetLimit(INT_MAX);
+    REQUIRE(mf9.CacheSize() == 10000); // int max is 2147483647 on most systems, no eviction as
+                                       // size is well within limit
 }
