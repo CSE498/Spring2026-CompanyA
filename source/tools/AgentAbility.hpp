dfa606db7ec0869a4a62d23b6a86372f62b1425e
ZodiacalBee <mylittlesnowyr88@gmail.com>
2026-03-06 20:00:54 -0500

    added missing comment in AgentAbility

--- source/tools/AgentAbility.hpp
+++ source/tools/AgentAbility.hpp
@@ -1,8 +1,9 @@
 /**
- * @file AgentAbility.h
+ * @file AgentAbility.hpp
  * @author lrima
  *
- *
+ * This is a preliminary placeholder file to represent some things the agent may or may not be able to do
+ * this information is used in path finding to know whether the agent can walk on certain tiles or not.
  */
 
 #ifndef SPRING2026_COMPANYA_GROUP_SPECIFIC_CONTENT_GROUP_02_AGENTABILITY_H
