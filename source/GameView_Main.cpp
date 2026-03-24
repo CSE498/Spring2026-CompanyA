9239ce5f5226ac56bf3af3df75bdf24a36656c22
DeniTepic <denitepic@gmail.com>
2026-03-24 18:56:24 -0400

    Fix gameview build and entry point issues

--- source/GameView_Main.cpp
+++ source/GameView_Main.cpp
@@ -1,10 +1,12 @@
 /**
-* @file gameview_main.cpp
+ * @file gameview_main.cpp
  * @author Group 17
  *
  * Entry point for the game.
  */
 
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
 #include "core/Game.hpp"
 
 int main()
