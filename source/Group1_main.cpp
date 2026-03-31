4d6613597e04130a82e866939984165ff8b6a8f1
jordansebagh <sebaghjo@msu.edu>
2026-03-31 12:38:05 -0400

    Main demo now showcases the new agents abilities in the demo

--- source/Group1_main.cpp
+++ source/Group1_main.cpp
@@ -1,39 +1,31 @@
 /**
- * This file is part of the Fall 2026, CSE 498, section 2, course project.
- * @brief A simplistic main file to demonstrate a system.
- * @note Status: PROPOSAL
+ * @file Group1_main.cpp
+ * @brief Group 1 demo entry: AIWorld + TrailblazerAgent +
+ *LearningExplorerAgent, then Run().
+ *
+ * @details Spawns Trailblazer (GOAP/combat) at (1,1) as 'T', and Explorer (BFS
+ *          coverage) at (3,1) as 'E'. For narrative docs, see docs/Group1.md.
+ *
+ * This file is part of the Spring 2026, CSE 498, section 2, course project.
  **/
 
 // Include the modules that we will be using.
-#include "Agents/EnemyAgent.hpp"
 #include "Agents/LearningExplorerAgent.hpp"
-#include "Agents/PacingAgent.hpp"
-#include "Interfaces/AIDebugInterface.hpp"
-#include "Interfaces/TrashInterface.hpp"
-#include "Worlds/MazeWorld.hpp"
+#include "Agents/TrailblazerAgent.hpp"
+#include "Worlds/AIWorld.hpp"
 
 using namespace cse498;
 
 int main() {
-  MazeWorld world;
+  AIWorld world;
 
-  auto &explorer = world.AddAgent<LearningExplorerAgent>("Explorer");
-  explorer.SetSymbol('E').SetLocation(WorldPosition{6, 1});
+  world.AddAgent<TrailblazerAgent>("Trailblazer")
+      .SetSymbol('T')
+      .SetLocation(WorldPosition{1, 1});
 
-  world.AddAgent<PacingAgent>("Guard 1")
-      .SetHorizontal()
-      .SetSymbol('G')
-      .SetLocation(WorldPosition{7, 7});
-
-  world.AddAgent<EnemyAgent>("Enemy")
-      .SetSymbol('X')
-      .SetLocation(WorldPosition{10, 8});
-
-  auto &observer = world.AddAgent<AIDebugInterface>("Observer");
-
-  observer.SetSymbol('O');
-  observer.SetLocation(WorldPosition{1, 1});
-  observer.SetMonitoredAgent(explorer.GetID());
+  world.AddAgent<LearningExplorerAgent>("Explorer")
+      .SetSymbol('E')
+      .SetLocation(WorldPosition{3, 1});
 
   world.Run();
 }
