ad696cb486f2864728142948cc2278d1ad580138
rcm7133 <moorma14@msu.edu>
2026-04-07 01:02:42 -0400

    Adjusted ResourceProducer from peer review comments. Added blank file for documentatation

--- source/core/InteractiveWorld/ResourceProducer.hpp
+++ source/core/InteractiveWorld/ResourceProducer.hpp
@@ -12,8 +12,6 @@
 #include "../InteractiveWorld/InteractiveWorldInventory.hpp"
 #include "Building.hpp"
 
-using namespace cse498;
-
 namespace cse498 {
 /**
  * Produces a single kind of resource over time. Rate of production is modified
@@ -42,16 +40,20 @@ public:
    * Get the current rate of production
    * @return current rate of production
    */
-  float GetRate() { return m_rate; }
+  float GetRate() const { return m_rate; }
   /**
    * Calculate the current rate of production
    */
   void CalculateRate() {
+    // If there is no building attached then just default to base rate
     if (!m_building) {
       m_rate = m_baseRate;
       return;
     }
-
+    // multiplier = 1 + current level * rate modifier
+    // If rate modifier is 0.25 then it will add .25 per level
+    // A level 3 building with a rate modifier of 0.25 will be
+    // 1 + 3 * 0.25 = 1.75x production
     float multiplier =
         1.0f + (m_building->GetCurrentLevel() * m_building->GetRateModifier());
     m_rate = m_baseRate * multiplier;
@@ -61,11 +63,16 @@ public:
    * Update the resource production
    */
   void Update() {
+    // Get current time
     auto now = std::chrono::steady_clock::now();
+    // Get delta time since last update
     std::chrono::duration<float> dt = now - m_lastTime;
     m_lastTime = now;
-
+    // add the amount of resources produced since last update as a float
     m_accumulator += m_rate * dt.count();
+    // If the amount added is greater than or equal to 1 then add the whole
+    // integer to the world inventory and subtract the amount added from the
+    // accumulator
     int whole = static_cast<int>(m_accumulator);
     if (whole > 0) {
       m_inventory.AddItem(m_outputType, whole);
@@ -78,8 +85,8 @@ private:
   std::shared_ptr<Building> m_building;   // Building modifying the output rate
   float m_baseRate{};                     // Base production rate
   float m_rate{};                         // Current Production Rate
-  float m_accumulator{0.0}; // Accumulated fraction of a resource over time
-  ItemType m_outputType;    // Type of item being produced
+  float m_accumulator{0.0f}; // Accumulated fraction of a resource over time
+  ItemType m_outputType;     // Type of item being produced
   std::chrono::steady_clock::time_point
       m_lastTime; // Last time checked for delta time
 
@@ -92,11 +99,5 @@ private:
     // Adjust rate
     CalculateRate();
   }
-
-  /**
-   * Set the world
-   * @param world pointer to world
-   */
-  void SetWorldInventory(InteractiveWorldInventory &inv) { m_inventory = inv; }
 };
 } // namespace cse498
